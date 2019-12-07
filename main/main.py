699da3266b21440150031b28a00f420b0c572c44
MiyabiTane <miyabitane@yahoo.co.jp>
2019-12-07 21:05:54 +0900

    顔であめが弾けた！楽しい！

--- main/main.py
+++ main/main.py
@@ -3,6 +3,9 @@ from jsk_recognition_msgs.msg import RectArray
 from jsk_recognition_msgs.msg import Rect
 import numpy as np
 import ame_movie
+import cv2
+import random
+import time as pytime
 
 message_arrived=False
 def position_cb(msg):
@@ -17,29 +20,218 @@ if __name__=='__main__':
         while not rospy.is_shutdown():
             if message_arrived:
                 print("message_arrived")
-                #rects=[info1,info2...]
-                if len(message_arrived.rects)>0:
-                    y_min=500
-                    umb_index=100
-                    #umbrella's y is min
-                    for i in range(len(message_arrived.rects)):
-                        if message_arrived.rects[i].y<y_min:
-                            umb_index=i
-                            y_min=message_arrived.rects[i].y
-                    print("rects={}".format(message_arrived.rects[umb_index]))
 
-                    um_pos_x=message_arrived.rects[umb_index].x+message_arrived.rects[umb_index].width/2
-                    um_pos_y=message_arrived.rects[umb_index].y+message_arrived.rects[umb_index].height/2
+                #full screen
+                #cv2.namedWindow('result', cv2.WINDOW_NORMAL)
+                #cv2.setWindowProperty('result', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+                #make ame
+                time=np.random.randint(0,250,20)
+                x_left=np.random.randint(100,1000,len(time))
+                #x_left=[579]*len(time)
+                #pitch candy:rain
+                images=np.random.randint(0,3,len(time))
+                size=np.zeros((len(time),2))
+                size[np.where(images==0)]=[140,140]
+                size[np.where(images!=0)]=[140,140]
+                fall_obs=[]
+                #info of each ame
+                for i in range(len(time)):
+                    fall_obs.append([images[i],x_left[i],0,size[i][0],size[i][1]])
 
-                    um_pos_x=np.clip(int(um_pos_x*65/24)-216,0,1300)
-                    um_pos_y=np.clip(int(um_pos_y*65/24),0,1300)
+                fps = 25
+                j=0
+                info=np.array([])
+                flip_info_r=np.array([])
+                flip_info_l=np.array([])
+                flip_info_rfar=np.array([])
+                flip_info_lfar=np.array([])
 
-                    ame_movie.fall_ame(um_pos_x,um_pos_y)
+                while j<=300:
+                    flip_rain_info=np.array([])
+                    start_time = pytime.time()
 
-                    print("x={},y={}".format(um_pos_x,um_pos_y))
+                    #rects=[info1,info2...]
+                    if len(message_arrived.rects)>0:
+                        y_min=500
+                        umb_index=100
+                        #umbrella's y is min
+                        for i in range(len(message_arrived.rects)):
+                            if message_arrived.rects[i].y<y_min:
+                                umb_index=i
+                                y_min=message_arrived.rects[i].y
+                        #print("rects={}".format(message_arrived.rects[umb_index]))
+
+                        um_pos_x=message_arrived.rects[umb_index].x+message_arrived.rects[umb_index].width/2
+                        um_pos_y=message_arrived.rects[umb_index].y+message_arrived.rects[umb_index].height/2
+
+                        um_pos_x=int(um_pos_x*65/24)-216
+                        um_pos_y=int(um_pos_y*65/24)
+                        #to satisfy the condition of flip ame
+                        um_pos_y=int(np.round(float(um_pos_y)/10)*10)
+
+                        print("x={},y={}".format(um_pos_x,um_pos_y))
+
+
+                    if j in time:
+                        num=np.where(time==j)[0][0]
+                        #print("num={}".format(num))
+                        info=info.tolist() #np.append is bad
+                        info.append(fall_obs[num])
+                        info=np.array(info)
+
+                    # think the relationship with umbrella
+                    if len(info)>0 and len(np.where(info[:,2]==um_pos_y)[0])>0:
+                        #remove candy from info
+                        num_f=np.where(info[:,2]==um_pos_y)
+                        keep=info[num_f[0][0]]
+                        keep=keep.tolist()
+
+                        if um_pos_x-70<=keep[1] and keep[1]<um_pos_x+100:
+                        #x_center of parabola
+                            keep.append(keep[1]+80)
+                            info=info.tolist()
+                            info.pop(num_f[0][0])
+                            info=np.array(info)
+                            #add candy to flip_info
+                            flip_info_r=flip_info_r.tolist()
+                            flip_rain_info=flip_rain_info.tolist()
+                            #candy only
+                            if keep[0]==0:
+                                keep[0]=4
+                                flip_rain_info.append(keep)
+                            else:
+                                flip_info_r.append(keep)
+
+                        elif um_pos_x-170<=keep[1] and keep[1]<um_pos_x-70:
+                            #x_centerof parabola
+                            keep.append(keep[1]-80)
+                            info=info.tolist()
+                            info.pop(num_f[0][0])
+                            info=np.array(info)
+                            #add candy to flip info
+                            flip_info_l=flip_info_l.tolist()
+                            flip_rain_info=flip_rain_info.tolist()
+                            #candy only
+                            if keep[0]==0:
+                                keep[0]=4
+                                flip_rain_info.append(keep)
+                            else:
+                                flip_info_l.append(keep)
+
+                        elif um_pos_x+100<=keep[1] and keep[1]<um_pos_x+200:
+                            #x_centerof parabola
+                            keep.append(keep[1]+100)
+                            info=info.tolist()
+                            info.pop(num_f[0][0])
+                            info=np.array(info)
+                            #add candy to flip info
+                            flip_info_rfar=flip_info_rfar.tolist()
+                            flip_rain_info=flip_rain_info.tolist()
+                            #candy only
+                            if keep[0]==0:
+                                keep[0]=4
+                                flip_rain_info.append(keep)
+                            else:
+                                flip_info_rfar.append(keep)
+
+                        elif um_pos_x-270<=keep[1] and keep[1]<um_pos_x-170:
+                            #x_centerof parabola
+                            keep.append(keep[1]-100)
+                            info=info.tolist()
+                            info.pop(num_f[0][0])
+                            info=np.array(info)
+                            #add candy to flip info
+                            flip_info_lfar=flip_info_lfar.tolist()
+                            flip_rain_info=flip_rain_info.tolist()
+                            #candy only
+                            if keep[0]==0:
+                                keep[0]=4
+                                flip_rain_info.append(keep)
+                            else:
+                                flip_info_lfar.append(keep)
+
+
+                        flip_info_r=np.array((flip_info_r),dtype=np.int)
+                        flip_info_l=np.array((flip_info_l),dtype=np.int)
+                        flip_info_rfar=np.array((flip_info_rfar),dtype=np.int)
+                        flip_info_lfar=np.array((flip_info_lfar),dtype=np.int)
+                        flip_rain_info=np.array((flip_rain_info),dtype=np.int)
+                    print("flip_rain_info={}".format(flip_rain_info))
+
+                    if len(flip_info_r)>0:
+                        flip_info_r[:,1]+=np.array([10]*len(flip_info_r),dtype=np.int)
+                        #make parabola
+                        num_3=np.where(flip_info_r[:,1]<flip_info_r[:,5])
+                        flip_info_r[num_3,2]-=np.array([20]*len(num_3[0]),dtype=np.int)
+                        num_4=np.where(flip_info_r[:,1]>flip_info_r[:,5])
+                        flip_info_r[num_4,2]+=np.array([10]*len(num_4[0]),dtype=np.int)
+                        #flip_info[:,2]=parabola(a=0.5,x_left=flip_info[:,1],y_up=flip_info[:,2],x_cen=flip_info[:,5],y_plus=10)
+                    flip_info_r=np.array([l for l in flip_info_r if (l[1]<1100 and l[2]<1100)])
+                    print("flip_info_r={}".format(flip_info_r))
+
+                    if len(flip_info_l)>0:
+                        flip_info_l[:,1]-=np.array([10]*len(flip_info_l),dtype=np.int)
+                        #make parabola
+                        num_5=np.where(flip_info_l[:,1]>flip_info_l[:,5])
+                        flip_info_l[num_5,2]-=np.array([20]*len(num_5[0]),dtype=np.int)
+                        num_6=np.where(flip_info_l[:,1]<flip_info_l[:,5])
+                        flip_info_l[num_6,2]+=np.array([10]*len(num_6[0]),dtype=np.int)
+                    flip_info_l=np.array([l for l in flip_info_l if (0<l[1] and l[2]<1100)])
+                    print("flip_info_l={}".format(flip_info_l))
+
+                    if len(flip_info_rfar)>0:
+                        flip_info_rfar[:,1]+=np.array([10]*len(flip_info_rfar),dtype=np.int)
+                        #make parabola
+                        num_7=np.where(flip_info_rfar[:,1]<flip_info_rfar[:,5])
+                        flip_info_rfar[num_7,2]-=np.array([5]*len(num_7[0]),dtype=np.int)
+                        num_8=np.where(flip_info_rfar[:,1]>flip_info_rfar[:,5])
+                        flip_info_rfar[num_8,2]+=np.array([5]*len(num_8[0]),dtype=np.int)
+                    flip_info_rfar=np.array([l for l in flip_info_rfar if (l[1]<1100 and l[2]<1100)])
+                    print("flip_info_rfar={}".format(flip_info_rfar))
+
+                    if len(flip_info_lfar)>0:
+                        flip_info_lfar[:,1]-=np.array([10]*len(flip_info_lfar),dtype=np.int)
+                        #make parabola
+                        num_9=np.where(flip_info_lfar[:,1]>flip_info_lfar[:,5])
+                        flip_info_lfar[num_9,2]-=np.array([5]*len(num_9[0]),dtype=np.int)
+                        num_6=np.where(flip_info_lfar[:,1]<flip_info_lfar[:,5])
+                        flip_info_lfar[num_6,2]+=np.array([5]*len(num_6[0]),dtype=np.int)
+                    flip_info_lfar=np.array(([l for l in flip_info_lfar if (0<l[1] and l[2]<1100)]),dtype=np.int)
+                    print("flip_info_lfar={}".format(flip_info_lfar))
+
+                    #remove fallen ame max:1100
+                    info=np.array(([i for i in info if i[2]<1100]),dtype=np.int)
+                    info=np.array(info)
+                    if len(info)>0:
+                        info[:,2]+=np.array([10]*len(info),dtype=np.int)
+                    print("info={}".format(info))
+
+
+                    img_1=ame_movie.Some2OnePicture('./images/sky.jpg',info)
+                    cv2.imwrite('./outputs/test_1201.jpg',img_1)
+                    img_2=ame_movie.Some2OnePicture('./outputs/test_1201.jpg',flip_info_r)
+                    cv2.imwrite('./outputs/test_1203.jpg',img_2)
+                    img_3=ame_movie.Some2OnePicture('./outputs/test_1203.jpg',flip_info_l)
+                    cv2.imwrite('./outputs/test_1207.jpg',img_3)
+                    img_4=ame_movie.Some2OnePicture('./outputs/test_1207.jpg',flip_info_rfar)
+                    cv2.imwrite('./outputs/test_1207_2.jpg',img_4)
+                    img_5=ame_movie.Some2OnePicture('./outputs/test_1207_2.jpg',flip_info_lfar)
+                    cv2.imwrite('./outputs/test_1207_3.jpg',img_5)
+                    img=ame_movie.Some2OnePicture('./outputs/test_1207_3.jpg',flip_rain_info)
+                    img = cv2.resize(img, (640,480))
+
+                    cv2.imshow("result",img)
+                    end_time = pytime.time()
+                    elapsed_time = end_time - start_time # sec
+                    print(elapsed_time) # 100
+                    wait_time = max(200 - int(elapsed_time*1000), 1) # msec
+                    print(wait_time)
+                    if cv2.waitKey(wait_time) >= 0:
+                        break
+                    j+=1
 
             message_arrived=False
-            rospy.sleep(0.5)
+            rospy.sleep(0.1)
 
     except rospy.ROSInterruptException:
         pass
