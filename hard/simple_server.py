41bc54fc896a6b5523a696905a3885b612acd358
MiyabiTane <miyabitane@yahoo.co.jp>
2019-12-21 13:31:37 +0900

    受け取った数字をprintするコールバック関数作った

--- hard/simple_server.py
+++ hard/simple_server.py
@@ -9,30 +9,19 @@ import math
 from pythonosc import dispatcher
 from pythonosc import osc_server
 
-
-def print_volume_handler(unused_addr, args, volume):
-    print("[{0}] ~ {1}".format(args[0], volume))
-
-
-def print_compute_handler(unused_addr, args, volume):
-    try:
-        print("[{0}] ~ {1}".format(args[0], args[1](volume)))
-    except ValueError:
-        pass
-
+def print_num(unused_addr,num):
+    print("num={}".format(num))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--ip",
-                        default="192.168.3.11", help="The ip to listen on")
+                        default='127.0.0.1', help="The ip to listen on")
     parser.add_argument("--port",
                         type=int, default=5005, help="The port to listen on")
     args = parser.parse_args()
 
     dispatcher = dispatcher.Dispatcher()
-    dispatcher.map("/filter", print)
-    dispatcher.map("/volume", print_volume_handler, "Volume")
-    dispatcher.map("/logvolume", print_compute_handler, "Log volume", math.log)
+    dispatcher.map("/filter", print_num)
 
     server = osc_server.ThreadingOSCUDPServer(
         (args.ip, args.port), dispatcher)
