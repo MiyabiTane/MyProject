02ab3ed01c1221fbd1e539dfdb5a7283b451b90d
MiyabiTane <miyabitane@yahoo.co.jp>
2019-12-21 16:12:14 +0900

    OSC通信追加

--- main/game_result.py
+++ main/game_result.py
@@ -2,7 +2,19 @@ import ame_movie
 import cv2
 import time
 import pygame
-import threading
+import argparse
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
+
+parser = argparse.ArgumentParser()
+#please change IP 
+parser.add_argument("--ip", default='127.0.0.1',
+                    help="The ip of the OSC server")
+parser.add_argument("--port", type=int, default=5005,
+                    help="The port the OSC server is listening on")
+args = parser.parse_args()
+
+client = udp_client.SimpleUDPClient(args.ip, args.port)
 
 #memo 1=flip_rain 2=pakkun 3=flip_high 4=flip_low
 def play_sound(music,sleep):
@@ -61,28 +73,42 @@ def game_result(point):
             break
         count+=1
         if sound_flag==0:
+            client.send_message("/filter", 5)
             play_sound('./sounds/dram.mp3',3)
             sound_flag=1
 
     sound_flag=0
     count=0
+    stop_flag=0
     while count<25:
         #show result
         if original_point>40:
             img=ame_movie.ResultPicture('./images/sky_nizi.jpg',info)
+            sound_flag=1
         elif original_point>20:
             img=ame_movie.ResultPicture('./images/sky_cloud.jpg',info)
+            sound_flag=2
         else:
             img=ame_movie.ResultPicture('./images/sky_rain.jpg',info)
+            sound_flag=3
         img = cv2.resize(img, (640,480))
         cv2.imshow("result",img)
         if cv2.waitKey(20) >= 0:
             break
         count+=1
-        if sound_flag==0:
-            play_sound('./sounds/result.mp3',4)
-            sound_flag=1
+        if sound_flag==1 and stop_flag==0:
+            client.send_message("/filter", 6)
+            play_sound('./sounds/result_rainbow.mp3',4)
+            stop_flag=1
+        elif sound_flag==2 and stop_flag==0:
+            client.send_message("/filter", 7)
+            play_sound('./sounds/result_cloud.mp3',4)
+            stop_flag=1
+        elif sound_flag==3 and stop_flag==0:
+            client.send_message("/filter", 8)
+            play_sound('./sounds/result_rain.mp3',5)
+            stop_flag=1
 
 
-game_result(30)
-#play_sound('./sound/result.mp3',3)
+#game_result(-30)
+#play_sound('./sounds/opening.mp3',5)
