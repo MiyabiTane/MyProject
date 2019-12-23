d296121f7707ccd65d2b79cd862c5623137de418
MiyabiTane <miyabitane@yahoo.co.jp>
2019-12-23 21:49:38 +0900

    modified

--- main/opening.py
+++ main/opening.py
@@ -20,69 +20,31 @@ args = parser.parse_args()
 client = udp_client.SimpleUDPClient(args.ip, args.port)
 
 def sound_control():
-    play_sound.play_sound('./sounds/opening.mp3',15)
-    #client.send_message("/filter", 9)
+    client.send_message("/filter", 9)
+    play_sound.play_sound('./sounds/opening.mp3',14.5)
 
 def image_control():
     cv2.namedWindow('result', cv2.WINDOW_NORMAL)
     cv2.setWindowProperty('result', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     count=0
     sound_flag=0
-    while count<7:
+    images=[[0,140,325,300,300],[1,310,350,300,300],[2,475,350,300,300],[3,690,340,300,300],[4,850,350,300,300],[5,0,350,300,300],[6,1000,350,300,300],[0]]
+    info=[]
+    while len(images)>0:
 
         print("count={}".format(count))
-        if count==0:
-            img=ame_movie.OpeningPicture('./images/sky.jpg',[[0,140,325,300,300]])
-            cv2.imwrite('./outputs/test_1223_1.jpg',img)
-            img=cv2.resize(img,(640,480))
-            cv2.imshow("result",img)
-
-        elif count==1:
-            img=ame_movie.OpeningPicture('./outputs/test_1223_1.jpg',[[1,310,350,300,300]])
-            cv2.imwrite('./outputs/test_1223_2.jpg',img)
-            img=cv2.resize(img,(640,480))
-            cv2.imshow("result",img)
-
-        elif count==2:
-            img=ame_movie.OpeningPicture('./outputs/test_1223_2.jpg',[[2,475,350,300,300]])
-            cv2.imwrite('./outputs/test_1223_3.jpg',img)
-            img=cv2.resize(img,(640,480))
-            cv2.imshow("result",img)
-
-        elif count==3:
-            img=ame_movie.OpeningPicture('./outputs/test_1223_3.jpg',[[3,690,340,300,300]])
-            cv2.imwrite('./outputs/test_1223_4.jpg',img)
-            img=cv2.resize(img,(640,480))
-            cv2.imshow("result",img)
-
-        elif count==4:
-            img=ame_movie.OpeningPicture('./outputs/test_1223_4.jpg',[[4,850,350,300,300]])
-            cv2.imwrite('./outputs/test_1223_5.jpg',img)
-            img=cv2.resize(img,(640,480))
-            cv2.imshow("result",img)
-
-        elif count==5:
-            img=ame_movie.OpeningPicture('./outputs/test_1223_5.jpg',[[5,0,350,300,300]])
-            cv2.imwrite('./outputs/test_1223_6.jpg',img)
-            img=cv2.resize(img,(640,480))
-            cv2.imshow("result",img)
-
-        elif count==6:
-            img=ame_movie.OpeningPicture('./outputs/test_1223_6.jpg',[[6,1000,350,300,300]])
-            cv2.imwrite('./outputs/test_1223_7.jpg',img)
-            img=cv2.resize(img,(640,480))
-            cv2.imshow("result",img)
+        img=ame_movie.OpeningPicture('./images/sky.jpg',info)
+        img=cv2.resize(img,(640,480))
+        cv2.imshow("result",img)
 
         if cv2.waitKey(1000) >= 0:
             break
+        info.append(images.pop(0))
         count+=1
 
-def opening():
-    thread1=threading.Thread(target=sound_control)
-    thread2=threading.Thread(target=image_control)
-    thread1.start()
-    thread2.start()
-    thread1.join()
-    thread2.join()
-
-opening()
+thread1=threading.Thread(target=sound_control)
+thread2=threading.Thread(target=image_control)
+thread1.start()
+thread2.start()
+thread1.join()
+thread2.join()
