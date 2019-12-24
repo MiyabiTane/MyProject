85d2cb2590f52bef52d73a3ac7edcc721e4fc27d
MiyabiTane <miyabitane@yahoo.co.jp>
2019-12-24 22:17:13 +0900

    thread使うと次のプログラムが動かなくなるので修正

--- main/opening.py
+++ main/opening.py
@@ -23,28 +23,27 @@ def sound_control():
     client.send_message("/filter", 9)
     play_sound.play_sound('./sounds/opening.mp3',14.5)
 
-def image_control():
-    cv2.namedWindow('result', cv2.WINDOW_NORMAL)
-    cv2.setWindowProperty('result', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-    count=0
-    sound_flag=0
-    images=[[0,140,325,300,300],[1,310,350,300,300],[2,475,350,300,300],[3,690,340,300,300],[4,850,350,300,300],[5,0,350,300,300],[6,1000,350,300,300],[0]]
-    info=[]
-    while len(images)>0:
+thread=threading.Thread(target=sound_control)
+thread.start()
 
-        print("count={}".format(count))
-        img=ame_movie.OpeningPicture('./images/sky.jpg',info)
-        img=cv2.resize(img,(640,480))
-        cv2.imshow("result",img)
+start=time.time()
+cv2.namedWindow('result', cv2.WINDOW_NORMAL)
+cv2.setWindowProperty('result', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+count=0
+sound_flag=0
+images=[[0,140,325,300,300],[1,310,350,300,300],[2,475,350,300,300],[3,690,340,300,300],[4,850,350,300,300],[5,0,350,300,300],[6,1000,350,300,300],[0]]
+info=[]
+while len(images)>0:
+    print("count={}".format(count))
+    img=ame_movie.OpeningPicture('./images/sky.jpg',info)
+    cv2.imwrite('./outputs/test_1223.jpg',img)
+    img=cv2.resize(img,(640,480))
+    cv2.imshow("result",img)
 
-        if cv2.waitKey(1000) >= 0:
-            break
-        info.append(images.pop(0))
-        count+=1
+    if cv2.waitKey(1000) >= 0:
+        break
+    info.append(images.pop(0))
+    count+=1
+end=time.time()
 
-thread1=threading.Thread(target=sound_control)
-thread2=threading.Thread(target=image_control)
-thread1.start()
-thread2.start()
-thread1.join()
-thread2.join()
+time.sleep(14.5-(end-start))
